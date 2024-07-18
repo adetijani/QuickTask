8867d0a0004dfc1c90b70b370899e89307b70e7f
Adebowale Tijani <tijanidebo@gmail.com>
2024-07-18 22:34:40 +0100

    Updated Readme file

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.util.HashSet;
 import java.util.Set;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //Array  example given in question:
         int[] array1 = {1, 3, 6, 4, 1, 2};
         int[] array2 = {5, -1, -3};
-        System.out.println("The smallest non-occuring integer in array1:" + findSmallestNonOccuringInteger(array1));
-        System.out.println("The smallest non-occuring integer in array2:" + findSmallestNonOccuringInteger(array2));
+
+        //The getSmallestNonOccuringInteger method is called with each array and printed out
+        System.out.println("The smallest non-occuring integer in array1:" + getSmallestNonOccuringInteger(array1));
+        System.out.println("The smallest non-occuring integer in array2:" + getSmallestNonOccuringInteger(array2));
     }
 
 
 
-    public static int findSmallestNonOccuringInteger(int[] array) {
+    public static int getSmallestNonOccuringInteger(int[] array) {
         Set<Integer> numbers = new HashSet<>();
         for (int num : array) {
             if (num > 0) {
                 numbers.add(num);
             }
         }
+        /* to find the smallest non occuring integer, we start with 1 and if 1 occurs we check 2 and keep
+         going until we find a positive integer that is not in the set and return it */
+
             int smallestNonOccuringInteger = 1;
             while (numbers.contains(smallestNonOccuringInteger)) {
                 smallestNonOccuringInteger++;

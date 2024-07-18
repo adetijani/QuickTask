c08e487c93bb23c86ffbfbf2c4bc7d2e7504a98b
Adebowale Tijani <tijanidebo@gmail.com>
2024-07-18 23:59:54 +0100

    Updated statement to view output in assending order

--- src/sqlSolution.sql
+++ src/sqlSolution.sql
@@ -1,4 +1,6 @@
 SELECT T1.id, T1.name, T1.age
 FROM T1
 LEFT JOIN T2 ON T1.id = T2.id AND T1.name = T2.name AND T1.age = T2.age
-WHERE T2.id IS NULL;
+WHERE T2.id IS NULL
+ORDER BY T1.id ASC;  -- Added ORDER BY clause
+
